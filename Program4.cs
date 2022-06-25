90762893fe332838e5d32ebc290ff4aa4b5f8c84
Kerem Nalbant <kerem.nalbant@hotmail.com>
2022-06-26 02:43:01 +0300

    2015 Contest solutions

---
+++ Program4.cs
@@ -0,0 +1,44 @@
+﻿//class Solution4
+//{
+//    public int solution(int[] A)
+//    {
+//        int n = A.Length;
+//        int res = 0;
+//        int[] depths = new int[n];
+//        int wall = A[0];
+
+//        for (int i = 1; i < n - 1; ++i)
+//        {
+//            if (A[i] > wall)
+//                wall = A[i];
+//            else
+//                depths[i] = wall - A[i];
+//        }
+
+//        wall = A[n - 1];
+
+//        for (int i = n - 2; i > 0; --i)
+//        {
+//            if (A[i] > wall)
+//            {
+//                wall = A[i];
+//                depths[i] = 0;
+//            }
+//            else
+//                depths[i] = Math.Min(wall - A[i], depths[i]);
+
+//            res = Math.Max(res, depths[i]);
+//        }
+//        return res;
+//    }
+//}
+//class Program4
+//{
+//    static void Main(string[] args)
+//    {
+//        int[] blocks = { 1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2 };
+//        Solution4 solution = new();
+//        int result = solution.solution(blocks);
+//        Console.WriteLine(result);
+//    }
+//}
---
+++ Program5.cs
@@ -0,0 +1,51 @@
+﻿class Solution5
+{
+    public int lengthOfLIS(long[] nums)
+    {
+        long[] temp = new long[nums.Length];
+        int idx = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int fnd = Array.BinarySearch(temp, 0, idx, nums[i]);
+
+            if (fnd < 0)
+            {
+                fnd = -fnd - 1;
+            }
+
+            if (fnd == idx)
+            {
+                idx++;
+            }
+
+            temp[fnd] = nums[i];
+        }
+        return idx;
+    }
+
+    public int solution(int[] A)
+    {
+        long bound = A.Max();
+        long[] withMirrors = new long[A.Length * 3];
+        for (int i = 0; i < A.Length; i++)
+        {
+            withMirrors[i * 3] = bound * 2 + A[i] + 1;
+            withMirrors[i * 3 + 1] = bound * 2 - A[i] + 1;
+            withMirrors[i * 3 + 2] = A[i];
+        }
+        return lengthOfLIS(withMirrors);
+    }
+
+}
+
+
+class Program4
+{
+    static void Main(string[] args)
+    {
+        int[] a = { 15, 13, 5, 7, 4, 10, 12, 8, 2, 11, 6, 9, 3 };
+        Solution5 solution = new();
+        int result = solution.solution(a);
+        Console.WriteLine(result);
+    }
+}
